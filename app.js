d6ce1c75a0786f103da4e9af33196b37ee272c08
Aleksandra <semenovalekshu@gmail.com>
2022-05-29 21:07:52 +0300

    refactor: rename blocks and elements with BEM according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -32,18 +32,21 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = 'task-list__item';
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-list__task-name';
 
     //Each elements, needs appending
-    checkBox.type="checkbox";
+    checkBox.type = "checkbox";
+    checkBox.classList = 'task-list__checkbox';
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task-list__text-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit button";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete button";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
